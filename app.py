3787d928ef16535468722bae662a458b6d3dae97
2025aa05649-hue <2025aa05649@wilp.bits-pilani.ac.in>
2026-02-11 14:12:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,16 @@ from sklearn.metrics import classification_report, confusion_matrix
 
 st.title("Income Prediction Classification App")
 
-model_choice = st.selectbox(
-    "Choose Model",
-    ["Logistic Regression", "Decision Tree", "KNN", "Naive Bayes", "Random Forest", "XGBoost"]
-)
+model_files = {
+    "Logistic Regression": "model/Logistic_Regression.pkl",
+    "Decision Tree": "model/Decision_Tree.pkl",
+    "KNN": "model/KNN.pkl",
+    "Naive Bayes": "model/Naive_Bayes.pkl",
+    "Random Forest": "model/Random_Forest.pkl",
+    "XGBoost": "model/XGBoost.pkl"
+}
+
+model = joblib.load(model_files[model_choice])
 
 uploaded_file = st.file_uploader("Upload CSV test file", type=["csv"])
 
