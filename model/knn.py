b3dd06d54f94cb3b32a0b5f7a0628538c656f4b7
2025aa05649-hue <2025aa05649@wilp.bits-pilani.ac.in>
2026-02-15 12:12:59 +0000

    updated F1 and MCC

--- model/knn.py
+++ model/knn.py
@@ -1,5 +1,5 @@
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, matthews_corrcoef
 
 
 def train_and_evaluate(X_train, X_test, y_train, y_test):
@@ -13,7 +13,8 @@ def train_and_evaluate(X_train, X_test, y_train, y_test):
         "Accuracy": accuracy_score(y_test, y_pred),
         "Precision": precision_score(y_test, y_pred, zero_division=0),
         "Recall": recall_score(y_test, y_pred, zero_division=0),
-        "F1-Score": f1_score(y_test, y_pred, zero_division=0)
+        "F1-Score": f1_score(y_test, y_pred, zero_division=0),
+        "MCC": matthews_corrcoef(y_test, y_pred)
     }
     
     return y_pred, metrics
